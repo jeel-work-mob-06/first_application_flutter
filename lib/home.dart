81829e29821a58d34d3f1c42ed6a886f172894be
Jeel Parekh <jeelparekh2024@gmail.com>
2025-12-15 17:54:43 +0530

    Radio Button with message print

--- lib/home.dart
+++ lib/home.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class HomeState extends StatefulWidget{ 
 
   String sEmail,sPassword;
-  HomeState(this.sEmail,this.sPassword);
+  HomeState(this.sEmail,this.sPassword, {super.key});
 
       @override
       HomeMain createState() => HomeMain();
 }
 
 class HomeMain extends State<HomeState>{
+
+  int iGroupValue = 4;
 
       @override
   Widget build(BuildContext context) {
@@ -104,9 +107,104 @@ class HomeMain extends State<HomeState>{
               ],
               ),
             ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                                 children: [
+                  Radio(
+                    value: 0,
+                    groupValue: iGroupValue,
+                    onChanged: (value){
+                  
+                        setState(() {
+                          iGroupValue = 0;
+                          setGender(0,"Male");
+                        });
+                        
+                    }
+                    ),
+                    Text("Male",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold
+                      ),
+                    ),
+                                ],
+                              ),
+                ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                children: [
+                  Radio(
+                    value: 1,
+                    groupValue: iGroupValue,
+                    onChanged: (value){
+              
+                        setState(() {
+                          iGroupValue = 1;
+                          setGender(1,"Female");
+                        });
+                        
+                    }
+                    ),
+                    Text("Female",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold
+                      ),
+                    ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                children: [
+                  Radio(
+                    value: 2,
+                    groupValue: iGroupValue,
+                    onChanged: (value){
+              
+                        setState(() {
+                          iGroupValue = 2;
+                          setGender(2,"Transgender");
+                        });
+                        
+                    }
+                    ),
+                    Text("Transgender",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold
+                      ),
+                    ),
+                ],
+              ),
+            ),
+
+            ],
+            ),
           ],
         ),
       ),
     );
   }
+
+  void setGender(value,message){
+
+    iGroupValue = value;
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.CENTER
+    );
+
+  }
+
 }
