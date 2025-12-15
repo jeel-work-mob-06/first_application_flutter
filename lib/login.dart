86861a60ba42d347855519525f24b5396d814b7e
Jeel Parekh <jeelparekh2024@gmail.com>
2025-12-15 14:38:42 +0530

    Login page with image view and Toast message With includinhg Dependency

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class LoginApp extends StatelessWidget{
   const LoginApp({super.key});
@@ -9,6 +10,7 @@ class LoginApp extends StatelessWidget{
     // TODO: implement build
 
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: LoginState(),
     );
   }
@@ -31,10 +33,79 @@ class LoginMain extends State<LoginState>{
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text("Login"),
+        title: Text(
+          "Login",
+          style: TextStyle(color: Colors.black,fontSize: 20.0,fontWeight: FontWeight.bold),
+          ),
         backgroundColor: const Color.fromARGB(255, 76, 175, 163),
       ),
-      body: Container(),
+      body: Container(
+        color: const Color.fromARGB(255, 168, 193, 205),
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset(
+                'asset/icons/application.png',
+                width: 100.0,
+                height: 100.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(30.0,50.0,30.0,0),
+                  child: TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0)
+                      ),
+                      hintText: "Enter Email Id:",
+                      labelText: "Email Id",
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(30.0,20.0,30.0,0),
+                  child: TextField(
+                    obscureText: true,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0)
+                      ),
+                      hintText: "Enter Password:",
+                      labelText: "Password",
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: Container(
+                    color: const Color.fromARGB(255, 98, 75, 202),
+                    width: 100.0,
+                    height: 40.0,
+                    child: TextButton(
+                      onPressed: (){
+                        print("Login Succesfull");
+                        Fluttertoast.showToast(
+                          msg:"Login Successfull",
+                          toastLength: Toast.LENGTH_SHORT,
+                        );
+                      },
+                       child: Text(
+                        "Login",
+                       style: 
+                       TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0
+                        ),
+                        ),
+                       ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
     );
 
   }
